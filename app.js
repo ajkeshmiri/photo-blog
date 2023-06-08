455710ab7be55932c7f1f9b687dd5052106dbbc8
ajkeshmiri <123808040+ajkeshmiri@users.noreply.github.com>
2023-06-08 19:56:55 +0000

    demo changes

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const yearButton = document.querySelectorAll('li');
-const post = document.querySelectorAll('.post');
-const span = document.querySelectorAll('span');
+// const yearButton = document.querySelectorAll('li');
+// const post = document.querySelectorAll('.post');
+// const span = document.querySelectorAll('span');
 
-function sortByYear(year) {
-    for (i = 0; i < span.length; i++) {
-        if (span[i].innerText = '2018') {
-            post[i].classList.toggle('hidden');
-        }
-    }
-}
+// function sortByYear(year) {
+//     for (i = 0; i < span.length; i++) {
+//         if (span[i].innerText = '2018') {
+//             post[i].classList.toggle('hidden');
+//         }
+//     }
+// }
 
-yearButton[1].addEventListener('click', sortByYear(2018));
+// yearButton[1].addEventListener('click', sortByYear(2018));
 
